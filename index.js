ff5ca30347b1335e564dc85fb06d0e2432f3ac23
James Garcia <colossus9@github.com>
2018-12-03 00:44:16 -0600

    Commented out issues.opened listener

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.exports = app => {
 
   app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    app.log('Received issues.opened event')
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+  //app.on('issues.opened', async context => {
+  //  app.log('Received issues.opened event')
+  //  const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+  //  return context.github.issues.createComment(issueComment)
+  //})
 
   app.on('label.created', async context => {
     app.log('Received label.created event')
