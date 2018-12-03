69d5920d8d771a1d8ff33df884333baf50c03d99
James Garcia <colossus9@github.com>
2018-12-03 00:52:15 -0600

    Added detailed handle to label creation payload

--- index.js
+++ index.js
@@ -9,11 +9,24 @@ module.exports = app => {
   //})
 
   app.on('label.created', async context => {
-    app.log('Received label.created event')
+    app.log('Received label event')
+
+    // Get payload data
     const owner = await context.payload.repository.owner.login
-    const repo = await context.payload.repository.name
-    const label = await context.payload.label.name
-    const issue = context.issue({ owner: owner, repo: repo, title: 'Label Created', body: 'A label was created' })
+    const repo_name = await context.payload.repository.name
+    const label_name = await context.payload.label.name
+    const label_action = await context.payload.action
+    const label_url = await context.payload.label.url
+    const label_color = await context.payload.label.color
+
+    // Generate new Issue object
+    const issue = context.issue({ owner: owner, 
+                                  repo: repo_name, 
+                                  title: 'Label ' + label_action + ' - ' + label_name, 
+                                  body: 'A label was ' + label_action + ':\n\n<kbd>'+label_name+'</kbd> `#'+label_color+'`\n\nSee the label at ' + label_url
+                                })
+    
+    // Create the new Issue
     return context.github.issues.create(issue)
   })
 }
